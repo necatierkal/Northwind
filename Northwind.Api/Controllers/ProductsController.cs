91bf8c796e06b2fb619f43168f88f4464d925b5b
Burcu BOLAT AKÇA <bbakca@kho.edu.tr>
2024-04-22 16:26:24 +0300

    Deneme

--- Northwind.Api/Controllers/ProductsController.cs
+++ Northwind.Api/Controllers/ProductsController.cs
@@ -23,6 +23,37 @@ namespace Northwind.Api.Controllers
 
 
             }
+
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Get(int id)
+        {
+            using (var context = new NorthwindContext())
+            {
+
+               var product =  context.Products.SingleOrDefault(x=>x.Id==id); 
+             
+                return Ok(product);
+
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult Delete(int id)
+        {
+            using (var context = new NorthwindContext())
+            {
+                context.Products.Remove(new Product
+                {
+                    Id = id
+                }); //audit log almıyorsak böyle kullanılabilir. Ya da audit log alıyorsak vt dan getirip onu silebiliriz.
+                context.SaveChanges();
+                return NoContent();//Sildikten sonran veri olmadığı için nocontent yani 204 döneriz.
+
+            }
+        }
         
     }
 }
